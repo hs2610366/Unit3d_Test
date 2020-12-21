c11125c04fe9dd67654044e8a4c94306d9b10f2a
hs2610366 <306108462@qq.com>
2020-12-21 16:12:26 +0800

    editor用的协程 自定义UIElement基类

---
+++ Pro/Assets/Editor/Common/Coroutine/EditorCoroutine.cs
@@ -0,0 +1,66 @@
+﻿/**  
+* 标    题：   EditorCoroutine.cs 
+* 描    述：    类型UnityEngine下的Coroutine
+* 创建时间：   2020年12月19日 16:05 
+* 作    者：   by. T.Y.Divak 
+* 详    细：    
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Divak.Script.Editor
+{
+    public class EditorCoroutine : IEnumerator
+    {
+        private Stack<IEnumerator> executionStack;
+
+        public EditorCoroutine(IEnumerator iterator)
+        {
+            this.executionStack = new Stack<IEnumerator>();
+            this.executionStack.Push(iterator);
+        }
+
+        public bool MoveNext()
+        {
+            IEnumerator i = this.executionStack.Peek();
+
+            if (i.MoveNext())
+            {
+                object result = i.Current;
+                if (result != null && result is IEnumerator)
+                {
+                    this.executionStack.Push((IEnumerator)result);
+                }
+
+                return true;
+            }
+            else
+            {
+                if (this.executionStack.Count > 1)
+                {
+                    this.executionStack.Pop();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            throw new System.NotSupportedException("This Operation Is Not Supported.");
+        }
+
+        public object Current
+        {
+            get { return this.executionStack.Peek().Current; }
+        }
+
+        public bool Find(IEnumerator iterator)
+        {
+            return this.executionStack.Contains(iterator);
+        }
+
+    }
+}
---
+++ Pro/Assets/Editor/Common/UIElements/CustomUIElement.cs
@@ -0,0 +1,54 @@
+﻿/**  
+* 标    题：   CustomUIElement.cs 
+* 描    述：    
+* 创建时间：   2020年12月17日 19:57 
+* 作    者：   by. T.Y.Divak 
+* 详    细：    
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.UIElements;
+
+namespace Divak.Script.Editor 
+{
+	public abstract class CustomUIElement
+    {
+        protected VisualTreeAsset mTreeAsset = null;
+        protected VisualElement mRoot = null;
+
+        protected string mUXML = null;
+
+        public void InitRoot()
+        {
+            EditorUI.GetVisualTreeAsset(mUXML, out mTreeAsset);
+            if (mTreeAsset == null)
+            {
+                MessageBox.Error("ExcelItem描述文件null");
+                return;
+            }
+            mRoot = mTreeAsset.CloneTree();
+        }
+
+        public void AddStyle(StyleSheet style)
+        {
+            if (style == null) return;
+            if (mRoot == null) return;
+            mRoot.styleSheets.Add(style);
+        }
+
+        public void SetParent(VisualElement parent)
+        {
+            if (mRoot == null) return;
+            if (parent == null)
+            {
+                MessageBox.Error("传入的VisualElement父节点为空");
+                return;
+            }
+            parent.Add(mRoot);
+        }
+
+    }
+}
